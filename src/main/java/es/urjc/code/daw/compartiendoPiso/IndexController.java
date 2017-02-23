f1ca8f3a2603916a2cc4501f6f0f8cbb6f849331
OscarSanchezSanchez <o.sanchezsa@alumnos.urjc.es>
2017-02-23 18:02:46 +0100

    -proyecto spring integrado a git

---
+++ src/main/java/es/urjc/code/daw/compartiendoPiso/IndexController.java
@@ -0,0 +1,107 @@
+package es.urjc.code.daw.compartiendoPiso;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String indexView(){
+		
+		return "index";
+		
+	}
+	
+	@RequestMapping("/disconnect")
+	public String disconnectView(){
+		
+		return "index";
+		
+	}
+	
+	@RequestMapping("/register")
+	public String registerView(){
+		
+		return "register";
+	}
+	
+	@RequestMapping("/signin")
+	public String signinView(){
+		
+		return "signin";
+		
+	}
+	
+	@RequestMapping("/offer-room/{id}")
+	public String offerRoomView(@PathVariable AtomicInteger id){
+		
+		return "offer-room";
+		
+	}
+	
+	@RequestMapping("/offer-house/{id}")
+	public String offerHouseView(@PathVariable AtomicInteger id){
+		
+		return "offer-house";
+		
+	}
+	
+	@RequestMapping("/contact")
+	public String contactView(){
+		
+		return "contact";
+		
+	}
+	
+	@RequestMapping("/user/{id}")
+	public String userView(@PathVariable AtomicInteger id){
+		
+		return "contact";
+		
+	}
+	
+	@RequestMapping("/user")
+	public String userView(){
+		
+		return "user";
+		
+	}
+	
+	@RequestMapping("/newAd")
+	public String newAnnouncementView(){
+		
+		return "newAd";
+		
+	}
+	@RequestMapping("/ad-modify")
+	public String updateAdView(){
+		
+		return "adModify";
+		
+	}
+	
+	@RequestMapping("/admin")
+	public String administratorView(){
+		
+		return "admin";
+		
+	}
+	
+	@RequestMapping("/reject")
+	public String rejectView(){
+		
+		return "admin";
+		
+	}
+	@RequestMapping("/accept-offer")
+	public String acceptOffer(){
+		
+		return "admin";
+		
+	}
+
+}
