49593652a1448f9c92b7b102e190e58bb8b54d3b
OscarSanchezSanchez <o.sanchezsa@alumnos.urjc.es>
2017-03-07 19:01:22 +0100

    -modificacion de usuario con logueo

--- src/main/java/es/urjc/code/daw/compartiendoPiso/User/UserRestController.java
+++ src/main/java/es/urjc/code/daw/compartiendoPiso/User/UserRestController.java
@@ -3,23 +3,29 @@ package es.urjc.code.daw.compartiendoPiso.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
 public class UserRestController {
 
 	@Autowired 
 	private UserRepository userRepository;
+	@Autowired 
+	private UserComponent userComponent;
 	
-	@RequestMapping(value="user/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<User> modifyUser(@PathVariable long id, @RequestBody User userUpdated){
-		User user = userRepository.findOne(id);
-		userUpdated.setId(id);
-		userUpdated = userRepository.saveAndFlush(userUpdated);
-		return new ResponseEntity<User>(user,HttpStatus.OK);
-	}
+//	@JsonView
+//	@RequestMapping(value="/user", method=RequestMethod.PUT)
+//	public ResponseEntity<User> modifyUser(@RequestBody User userUpdated){
+//		if(userComponent.isLoggedUser()){
+//			userUpdated = userRepository.saveAndFlush(userUpdated);
+//			return new ResponseEntity<User>(userUpdated,HttpStatus.OK);
+//		}else{
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
 }
