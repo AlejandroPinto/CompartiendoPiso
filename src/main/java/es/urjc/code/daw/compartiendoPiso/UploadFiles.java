bc1dc4734614b32765289ba330022a1fbaea3bce
Nazaret7 <nazaretfyq@hotmail.com>
2017-03-12 19:37:55 +0100

    Volver a modificar

--- src/main/java/es/urjc/code/daw/compartiendoPiso/UploadFiles.java
+++ src/main/java/es/urjc/code/daw/compartiendoPiso/UploadFiles.java
@@ -1,13 +1,26 @@
 package es.urjc.code.daw.compartiendoPiso;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+
 public class UploadFiles {
-	private static final String FILES_FOLDER = "src/main/resources/static/img/users/";
+	private static final String FILES_FOLDER = "imgs/users/";
+	public final static String USER_AVATAR = "user-avatar";
+	public final static String OFFER_IMGS = "offer-imgs";
 
 	private List<String> imageTitles = new ArrayList<>();
 	
@@ -54,5 +67,49 @@ public class UploadFiles {
 		}
 		return num;
 	}
+	
+	@RequestMapping("/image/{user_id}/{filename}")
+	public void handleFileDownload(@PathVariable String user_id,@PathVariable String filename, 
+			 HttpServletResponse res) throws FileNotFoundException, IOException {
+
+		File file = new File(FILES_FOLDER, "0.jpg");
+
+		if (file.exists()) {
+			res.setContentType("image/jpeg");
+			res.setContentLength(new Long(file.length()).intValue());
+			FileCopyUtils.copy(new FileInputStream(file), res.getOutputStream());
+		} else {
+			res.sendError(404, "File" + "0" + "(" + file.getAbsolutePath() + ") does not exist");
+		}
+	}
+		
+//	@RequestMapping("/image/{generatedId}/{fileName}")
+//	public void handleFileDownload(@PathVariable String type, @PathVariable String generatedId,
+//			@PathVariable String fileName, HttpServletResponse res) throws FileNotFoundException, IOException {
+//
+//		String filePath = null;
+//		
+//		File file = new File(FILES_FOLDER, fileName+".jpg");
+//
+////		switch (type) {
+////		case USER_AVATAR:
+////			filePath = FILES_FOLDER + "/" + fileName + ".jpeg";
+////			break;
+////		case OFFER_IMGS:
+////			filePath = FILES_FOLDER+ "/" + generatedId + "/" + fileName + ".jpeg";
+////			break;
+////		default:
+////			break;
+////		}
+////		File file = new File(filePath);
+//
+//		if (file.exists()) {
+//			res.setContentType("image/jpeg");
+//			res.setContentLength(new Long(file.length()).intValue());
+//			FileCopyUtils.copy(new FileInputStream(file), res.getOutputStream());
+//		} else {
+//			res.sendError(404, "File" + fileName + "(" + file.getAbsolutePath() + ") does not exist");
+//		}
+//	}
 	
 }
