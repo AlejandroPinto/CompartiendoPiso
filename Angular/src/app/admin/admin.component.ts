10e9c3837320cde4069ffece3fabc296fd1c3456
Nazaret7 <nazaretfyq@hotmail.com>
2017-04-17 11:04:31 +0200

    Subida de ficheros

---
+++ Angular/src/app/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'admin',
+  templateUrl: './admin.component.html'
+})
+
+export class AdminComponent {
+}
