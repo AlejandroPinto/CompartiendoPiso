7bd68574c000785716fdc11d0af56556ee167837
Nazaret7 <nazaretfyq@hotmail.com>
2017-04-21 01:32:15 +0200

    Arreglado errores con el Login

--- Angular/src/app/signin/signin.service.ts
+++ Angular/src/app/signin/signin.service.ts
@@ -27,58 +27,45 @@ export class SigninService {
         return "Basic " + btoa(username + ":" + password);
   }
 
-  logIn(username: string, password: string) {
-    console.log("logIn");
-    this.http.sessionData.authToken = this.generateAuthString(username,password);
-    this.http.sessionData.isLogged = true;
+//   logIn(username: string, password: string) {
+//     console.log("logIn");
+//     this.http.sessionData.authToken = this.generateAuthString(username,password);
+//     this.http.sessionData.isLogged = true;
 
-    return this.http.get(BASE_URL_Login).map(
-        response => {
-          this.userService.getUser(username).subscribe(
-            user => {
-              this.http.sessionData.userLogged = user;
-              this.http.sessionData.isLogged = true;
-              // if(this.http.sessionData.userLogged.roles.indexOf("ADMIN") > -1){
-              //   this.http.sessionData.isAdmin = true;
-              // }
-            },
-            error =>{
-              console.log(error)
-              this.http.sessionData.isLogged = false;
-            } 
+//     return this.http.get(BASE_URL_Login).map(
+//         response => {
+//           this.userService.getUser(username).subscribe(
+//             user => {
+//               this.http.sessionData.userLogged = user;
+//               this.http.sessionData.isLogged = true;
+//               // if(this.http.sessionData.userLogged.roles.indexOf("ADMIN") > -1){
+//               //   this.http.sessionData.isAdmin = true;
+//               // }
+//             },
+//             error =>{
+//               console.log(error)
+//               this.http.sessionData.isLogged = false;
+//             } 
             
-          );
-      })
-      .catch(error => Observable.throw('Server error'));
-  }
+//           );
+//       })
+//       .catch(error => Observable.throw('Server error'));
+//   }
 
-//   // logOut() {
-//   //   console.log("logOut");
-//   //   // let headers: Headers = new Headers();
-//   //   // headers.append('Authorization', 'Basic ' + this.authCreds);
-//   //   return this.http.get(BASE_URL + 'logOut')
-//   //     .map(response => {
-//   //       this.isLogged = false;
-//   //       this.userLogged = null;
-//   //       return true;
-//   //     })
-//   //     .catch(error => Observable.throw('Server error'));
-//   // }
-
-  logOut(){
-        console.log("logOut");
-        return this.http.get(BASE_URL_Logout)
-        .map(
-            response => {
-                this.http.sessionData.userLogged = null;
-                console.log("logOut");
-                console.log(this.http.sessionData.userLogged);
-                this.http.sessionData.isLogged = false;
-                console.log("logOut");
-                console.log( this.http.sessionData.isLogged);
-            }
-        );
-    }
+//   logOut(){
+//         console.log("logOut");
+//         return this.http.get(BASE_URL_Logout)
+//         .map(
+//             response => {
+//                 this.http.sessionData.userLogged = null;
+//                 console.log("logOut");
+//                 console.log(this.http.sessionData.userLogged);
+//                 this.http.sessionData.isLogged = false;
+//                 console.log("logOut");
+//                 console.log( this.http.sessionData.isLogged);
+//             }
+//         );
+//     }
 
     public IsLogged() {
         return this.http.sessionData.isLogged;
